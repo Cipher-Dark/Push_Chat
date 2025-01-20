b79b4509e3b298b633745048cec8ac5b476cb912
Cipher-Dark <iamsofficials@gmail.com>
2025-01-20 14:24:07 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/sagar/push_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sagar.push_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/nav_bar/nav_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:push_chat/screens/home/home_scree.dart';
+import 'package:push_chat/screens/profile/profile_screen.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          color: Colors.transparent,
+          height: 60,
+          child: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home_outlined),
+                text: "Home",
+              ),
+              Tab(
+                icon: Icon(Icons.person_outline),
+                text: "Profile",
+              )
+            ],
+            indicatorColor: Colors.blue,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            HomeScree(),
+            ProfileScreen()
+          ],
+        ),
+      ),
+    );
+  }
+}
